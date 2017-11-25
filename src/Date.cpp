399684c278804b5f8e2b445f351bdd0ece0893b4
pseute <pseut001@odu.edu>
2017-11-25 18:22:14 -0500

    optimized isLeapYear calculation

--- src/Date.cpp
+++ src/Date.cpp
@@ -149,20 +149,7 @@ int Date::getDaysInMonth()
 
 bool Date::isLeapYear()
 {
-    if(year % 4 == 0)
-    {
-        if(year % 100 == 0)
-        {
-            if(year % 400 == 0)
-                return true;
-            else
-                return false;
-        }
-        else
-            return true;
-    }
-    else
-        return false;
+    return year % 4 == 0 && (year % 400 == 0 || year % 100 != 0);
 }
 
 void Date::incrementMonth()
